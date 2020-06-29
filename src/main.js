13d3c9e86f32c3f30adc9810cf6b9ea294b6486f
k3vnchen <k3vnchen@gmail.com>
2020-06-29 13:57:06 -0400

    Add threejs stuff

--- src/main.js
+++ src/main.js
@@ -2,70 +2,61 @@ import 'normalize.css';
 import * as THREE from 'three';
 import image360 from '../assets/images/dlmfvr.png';
 
-// var camera, scene, renderer;
+var camera, scene, renderer, mesh;
 
-// init();
+window.addEventListener('DOMContentLoaded', main);
 
-// function init() {
-//     var mesh;
-
-//     camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 1100 );
-//     camera.target = new THREE.Vector3( 0, 0, 0 );
-
-//     scene = new THREE.Scene();
-
-//     var geometry = new THREE.SphereBufferGeometry( 500, 60, 40 );
-//     // invert the geometry on the x-axis so that all of the faces point inward
-//     geometry.scale( - 1, 1, 1 );
-
-//     var material = new THREE.MeshBasicMaterial({
-//         map: THREE.TextureLoader().load(image360)
-//     });
-
-//     mesh = new THREE.Mesh( geometry, material );
-//     scene.add( mesh );
-
-//     renderer = new THREE.WebGLRenderer();
-//     renderer.setPixelRatio( window.devicePixelRatio );
-//     renderer.setSize( window.innerWidth, window.innerHeight );
-
-//     // document.addEventListener( 'mousemove', onPointerMove, false );
-//     window.addEventListener( 'resize', onWindowResize, false );
-// }
-
-
-init()
-animate();
-
-var scene, camera, renderer, cube
+function main() {
+    init();
+    animate();
+}
 
 function init() {
+    var container = document.getElementById('three')
+    console.log("hello")
+    console.log(container)
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100);
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    var geometry = new THREE.SphereBufferGeometry(100, 60, 40);
+    geometry.scale(- 1, 1, 1);
+
+    var texture = new THREE.TextureLoader().load(image360)
+    var material = new THREE.MeshBasicMaterial({map: texture});
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry( 2, 2, 2 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x2d2922 } );
-
-    cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-
-    camera.position.z = 5;
-    window.addEventListener( 'resize', onWindowResize, false );
+    // document.addEventListener('mousemove', onPointerMove, false);
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function animate() {
-    requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    update();
+}
+
+function update() {
+    // lat = Math.max(- 85, Math.min(85, lat));
+    // phi = THREE.MathUtils.degToRad(90 - lat);
+    // theta = THREE.MathUtils.degToRad(lon);
+
+    // camera.target.x = 500 * Math.sin(phi) * Math.cos(theta);
+    // camera.target.y = 500 * Math.cos(phi);
+    // camera.target.z = 500 * Math.sin(phi) * Math.sin(theta);
+    // camera.lookAt(camera.target);
+
+    renderer.render(scene, camera);
 }
 
 function onWindowResize(){
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer.setSize(window.innerWidth, window.innerHeight);
 }
