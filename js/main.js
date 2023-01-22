bd297cf85a5da0dd86c94f0bcf82d8974f7244cd
Kevin Chen <kchen1250@gmail.com>
2023-01-22 14:37:37 -0500

    js tweaks

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 $( "[data-project]" ).hover(
   function() {
     const project = $( this ).data( "project" );
-    let work_el = $( "#work" ).find( "[data-project='" + project + "']" ).first();
-    if (this !== work_el[0]) {
-      let a = work_el.find( "a" );
+    let projects_el = $( "#projects" ).find( "[data-project='" + project + "']" ).first();
+    if (this !== projects_el[0]) {
+      let a = projects_el.find( "a" );
       a.css("background-color", a.css("color"));
-      work_el.find( "a > *").css("color", "white");
+      projects_el.find( "a > *").css("color", "white");
     }
     $( "#image-grid" ).find( "[data-project='" + project + "']" ).addClass("selected");
   },
   function() {
     const project = $( this ).data( "project" );
-    let work_el = $( "#work" ).find( "[data-project='" + project + "']" ).first();
-    if (this !== work_el[0]) {
-      let a = work_el.find( "a" );
-      work_el.find( "a > *").css("color", "");
+    let projects_el = $( "#projects" ).find( "[data-project='" + project + "']" ).first();
+    if (this !== projects_el[0]) {
+      let a = projects_el.find( "a" );
+      projects_el.find( "a > *").css("color", "");
       a.css("background-color", "");
     }
     $( "#image-grid" ).find( "[data-project='" + project + "']" ).removeClass("selected");
