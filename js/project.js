d748b953c70540a4a524030a43b73e8c32ee83f4
Kevin Chen <kchen1250@gmail.com>
2023-07-16 16:30:59 -0400

    Add zoom-in/out cursor on img hover

--- js/project.js
+++ js/project.js
@@ -1,11 +1,11 @@
 /*
-    O-R-G inc.
+  O-R-G inc.
 
-    windowfull object
-    screenfull.js shim for iOS safari
-    see https://github.com/sindresorhus/screenfull.js/
+  windowfull object
+  screenfull.js shim for iOS safari
+  see https://github.com/sindresorhus/screenfull.js/
 
-    windowfull.js copied from https://www.stuart-bertolotti-bailey.org/
+  windowfull.js copied from https://www.stuart-bertolotti-bailey.org/
 */
 
 (function () {
@@ -18,43 +18,45 @@
   var content = document.getElementById('content');
   var footer = document.getElementsByTagName('footer')[0];
 
-  document.body.style.position = 'relative';  /* reqd ios overflow: hidden */
+  document.body.style.position = 'relative';  /* read ios overflow: hidden */
 
   var windowfull = {
-      request: function (element) {
-          document.body.style.overflow = 'hidden';
-          footer.style.display = 'none';
-          fullwindow.style.display = 'block';
-          content.classList.toggle('edge-fade');
-          element.classList.toggle('fullwindow');
-          element.style.objectPosition = 'center center';
-      },
-      exit: function (element) {
-          document.body.style.overflow = '';
-          footer.style.display = '';
-          fullwindow.style.display = 'none';
-          content.classList.toggle('edge-fade');
-          element.classList.toggle('fullwindow');
-          element.style.objectPosition = '';
-      },
-      toggle: function (element) {
-          return this.isFullwindow ? this.exit(element) : this.request(element);
-      }
+    request: function (element) {
+      document.body.style.overflow = 'hidden';
+      footer.style.display = 'none';
+      fullwindow.style.display = 'block';
+      content.classList.toggle('edge-fade');
+      element.classList.toggle('fullwindow');
+      element.style.objectPosition = 'center center';
+      element.style.cursor = 'zoom-out';
+    },
+    exit: function (element) {
+      document.body.style.overflow = '';
+      footer.style.display = '';
+      fullwindow.style.display = 'none';
+      content.classList.toggle('edge-fade');
+      element.classList.toggle('fullwindow');
+      element.style.objectPosition = '';
+      element.style.cursor = '';
+    },
+    toggle: function (element) {
+      return this.isFullwindow ? this.exit(element) : this.request(element);
+    }
   };
 
   Object.defineProperties(windowfull, {
-      isFullwindow: {
-          get: function () {
-              // check if currently fullwindow
-              // (by presence of class?
-              // or presence of div)
-              // return true;
-              // return Boolean(document[fn.fullscreenElement]);
-              // return Boolean(!(document.getElementById('fullwindow')));
-              // return Boolean(document.getElementById('fullwindow'));
-              return Boolean(fullwindow.style.display == 'block');
-          }
+    isFullwindow: {
+      get: function () {
+        // check if currently fullwindow
+        // (by presence of class?
+        // or presence of div)
+        // return true;
+        // return Boolean(document[fn.fullscreenElement]);
+        // return Boolean(!(document.getElementById('fullwindow')));
+        // return Boolean(document.getElementById('fullwindow'));
+        return Boolean(fullwindow.style.display == 'block');
       }
+    }
   });
 
   if (isCommonjs) {
@@ -65,7 +67,7 @@
 })();
 
 
-var imgs = document.querySelectorAll('img,video');
+var imgs = document.querySelectorAll('img');
 var i;
 for (i = 0; i < imgs.length; i++) {
 //   console.log('screenfull');
