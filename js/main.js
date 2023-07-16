f5071c7be872b26e01ae0e4ae97a64fe292639bc
Kevin Chen <kchen1250@gmail.com>
2023-07-16 19:35:15 -0400

    Add filter

--- js/main.js
+++ js/main.js
@@ -21,3 +21,32 @@ $( "[data-project]" ).hover(
     $( "#image-grid" ).find( "[data-project='" + project + "']" ).removeClass("selected");
   }
 );
+
+
+const buttonNames = ["design", "programming"];
+
+$( "button" ).on("click", function(event) {
+  const numSelected = $( "button.selected" ).length;
+  const element = $( this );
+  const buttonName = element.attr("name");
+
+  if (numSelected == 1 && element.hasClass("selected")) {
+    console.log("hi");
+    element.removeClass("selected");
+    $( "#projects, #image-grid" ).find("*").css({ "opacity": "", "filter": "" });
+    return;
+  }
+
+  element.addClass("selected");
+
+  for (const name of buttonNames) {
+    if (name === buttonName) {
+      $( "#projects, #image-grid" ).find(`.color-${name}`).css({ "opacity": "", "filter": "" });
+    } else {
+      $( "button[name='" + name + "']" ).removeClass("selected");
+      $( "#projects" ).find(`.color-${name}`).css({ "opacity": "0.25", "filter": "blur(2px)" });
+      $( "#image-grid" ).find(`.color-${name}`).css({ "opacity": "0.1", "filter": "blur(4px)" });
+    }
+  }
+});
+
